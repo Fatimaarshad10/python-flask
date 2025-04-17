b07bed5b439eb166f378ae7a0cee434f5511494a
Fatimaarshad10 <fatimaarshad091@gmail.com>
2025-04-17 23:24:46 +0500

    class no 03

---
+++ classno03/first.py
@@ -0,0 +1,37 @@
+# for i in range(1,21):
+#     if i % 2 != 0:
+#         print(i)
+
+# def  for_even_odd(start , end , option):
+#     if(option == 'even'):
+#         print("Even Number:")
+#         for i in range(start,end):
+#             if i % 2 == 0:
+#                 print(i)
+#     else :
+#         print("Odd Number:")
+#         for i in range(start, end):
+#             if i % 2 != 0:
+#                 print(i)
+
+
+# start = int(input('Start value ='))
+# end = int(input('End value ='))
+# option = input('Select Even or odd = ')
+# def for_even_odd(starting , ending , options):
+#     for i in range(starting , ending ):
+#         if (options == 'even'):
+#             if i % 2 == 0:
+#                 print(i)
+#         else:
+#             if i % 2 != 0:
+#                 print(i)
+# for_even_odd(start , end , option)
+
+# password generator
+
+# a = 10
+# b = 15
+# c = 20
+# a,b,c = (b,c,a)
+# print(a,b,c)
